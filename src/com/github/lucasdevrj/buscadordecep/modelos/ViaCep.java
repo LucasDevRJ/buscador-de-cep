34eb122c6043f9154c3a8803a290990e15c13cab
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-19 22:21:31 -0300

    adição do record ViaCep

---
+++ src/com/github/lucasdevrj/buscadordecep/modelos/ViaCep.java
@@ -0,0 +1,5 @@
+package com.github.lucasdevrj.buscadordecep.modelos;
+
+public record ViaCep(String cep, String logradouro, String complemento,
+                     String bairro, String localidade, String uf) {
+}
