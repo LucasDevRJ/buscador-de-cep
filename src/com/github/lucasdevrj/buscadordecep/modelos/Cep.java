420a956e448784565347525694a4dc111780ae15
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-19 22:17:28 -0300

    adição de todos os atributos de Cep

--- src/com/github/lucasdevrj/buscadordecep/modelos/Cep.java
+++ src/com/github/lucasdevrj/buscadordecep/modelos/Cep.java
@@ -1,4 +1,14 @@
 package com.github.lucasdevrj.buscadordecep.modelos;
 
 public class Cep {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 }
